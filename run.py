f69308a9726e5b7a1180749207afbd3eb4d62b3c
abalint <theandrewbalint@gmail.com>
2024-06-12 15:47:27 -0600

    Update run.py

--- run.py
+++ run.py
@@ -9,19 +9,26 @@ def run():
         for i in range(split):
             outLine = removeChars(lines[i])
             file.write(outLine)
+            if i % 100000 == 0:
+                print(str(i) + "/" + str(len(lines)))
     with open("file2.csv", "w", encoding="utf8") as file:
         for i in range(split, len(lines)):
             outLine = removeChars(lines[i])
             file.write(outLine)
+            if i % 100000 == 0:
+                print(str(i) + "/" + str(len(lines)))
+    print("done")
     return
 
+
 def removeChars(line):
-    allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 - ! # ( ) [ ] { } . , : ^ \\ /"
+    allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 - ! # ( ) [ ] { } . , : ^ \\ / \" \n"
     for char in line:
         if char not in allowedChars:
             print("Removing: " + char)
-            line = line.replace(char, "") + "\n"
+            line = line.replace(char, "")
     return line
+
 
 if __name__ == '__main__':
     run()
